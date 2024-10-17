f3a4e549e1f65ad82fc54b5bb262714ff30cd85e
Dany Daniel <68875920+ddanielos@users.noreply.github.com>
2024-10-17 04:57:22 +0000

    cambio de credenciales_v2

--- src/lib/cognitoConfig.js
+++ src/lib/cognitoConfig.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: 'us-east-2_WWrLdmDvJ',
-    ClientId: '3nhmnd8t8145dfc9hbd3tq83v6'
+    UserPoolId: 'us-east-2_3z5nTvGeB',
+    ClientId: '52j6m45mf20v8jl7c4gbr7fqfc'
 };
 
 export const userPool = new CognitoUserPool(poolData);
