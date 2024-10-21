1bebc4d5e3f441c4d3cfb3f90a1e3cd504b6be16
Dany Daniel <68875920+ddanielos@users.noreply.github.com>
2024-10-21 04:06:34 +0000

    limpieza de componentes

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,6 @@
-/*
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { useAuth } from '../hooks/useAuth'
-
-export default function Home() {
-  const router = useRouter()
-  const { isAuthenticated } = useAuth()
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      router.push('/dashboard')
-    } else {
-      router.push('/login')
-    }
-  }, [isAuthenticated, router])
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Bienvenido al sistema de Control de encendido de instancias ERP Yucay
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Redirigiendo...
-          </p>
-        </div>
-      </div>
-    </div>
-  )
-}
-*/
-
 import Logo from "@/ui/logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { lusitana } from "@/ui/fonts";
-import Image from "next/image";
 
 export default function Page() {
   return (
@@ -60,10 +22,6 @@ export default function Page() {
                 <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
               </Link>
             </div>
-
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Redirigiendo...
-            </p>
           </div>
         </div>
       </div>
